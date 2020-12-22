1c0a152750f1c3f1a9c777659e126d16769e7246
ZCAasd123 <823998314@qq.com>
2020-12-22 09:48:44 +0800

    处理重定向错误

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Layout from "@/layout";
+//处理Redirected when going from "" to "" via a navigation guard.的报错
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
 Vue.use(VueRouter);
 
 export const constantRoutes = [
