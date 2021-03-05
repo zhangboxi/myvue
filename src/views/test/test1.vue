cf95b8025c7827b0929b81718faa853434c9c8f8
ZCAasd123 <823998314@qq.com>
2021-03-05 16:05:25 +0800

    两种图片放置位置测试

--- src/views/test/test1.vue
+++ src/views/test/test1.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>test1</div>
+  <div>
+    test1
+    <img :src="imgUrl" />
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgUrl: require("@/assets/logo.png"),
+      id: "1"
+    };
+  }
+};
+</script>
--- src/views/test/test2.vue
+++ src/views/test/test2.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    test2 div
+    test2
+    <img :src="`${publicPath}img/logo.png`" />
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      // imgUrl: require("/public/img/logo.png"),
+      publicPath: process.env.BASE_URL,
+      id: "1"
+    };
   }
 };
 </script>
